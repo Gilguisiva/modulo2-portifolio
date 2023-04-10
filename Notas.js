1a23fb0864e4f58ab2d92cb3e99878d6689f19ee
Gilguisilva76 <gilguisilva@yahoo.com.br>
2023-04-10 13:16:45 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   Notas.js

---
+++ Notas.js
@@ -0,0 +1,22 @@
+
+/* Nota escolar*/
+
+"Aluna: Paula"
+
+var nota1 = 8
+var nota2 = 5
+var mediasnotas = (nota1 + nota2)/2
+console.log(mediasnotas)
+if(mediasnotas >= 6)
+console.log("Aprovados")
+else
+console.log("Reprovados")
+
+
+
+
+/* Valor > = 7 serão Aprovados
+    Valor< = 6 serão reprovados*/
+
+
+
