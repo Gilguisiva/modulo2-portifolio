5e4f95f4c6481fc78804a37998bdf9a6a2af93c1
Gilguisilva76 <gilguisilva@yahoo.com.br>
2023-04-18 15:13:00 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   projeto1.js

--- projeto1.js
+++ projeto1.js
@@ -39,6 +39,18 @@ while(i<=10){
 console.log(n +"x" + i +"="+ (n*i));
 i = i + 1;
 }
+//tabuada
+
+const valores =[10.50,11.83,16.90,120,50];
+
+for( let i = 0; i < valores.length;i++){
+console.log("valor da array"+ valores [i]);
+console.log("Seu número inteiro é:"+Math.trunc(valores[i]))
+console.log("Seu quadrado é:"+ Math.pow(valores[i],2))
+console.log("Sua raiz quadrada é:"+ Math.sqrt(valores[i]))
+console.log("Arredondado para cima,ele fica assim:"+Math.ceil(valores[i]))
+console.log("Arredondado para baixo, ele fica assim:"+ Math.floor(valores [i]))
+}
 
 
 
